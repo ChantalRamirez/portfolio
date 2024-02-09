c8a160be4fb376811d1ebb9a92fe94b75e7e4249
ChantalRamirez <ramirez_yessica@ucol.mx>
2024-02-08 22:15:27 -0800

    section welcome-hero completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -100,11 +100,13 @@ $(document).ready(function(){
         $(window).load(function(){
         	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
+            $(".single-about-img").removeClass("animated fadeInDown").css({'opacity':'0'});
         });
 
         $(window).load(function(){
         	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
+			$(".single-about-img").addClass("animated fadeInDown").css({'opacity':'0'});
         });
 
 });	
